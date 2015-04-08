cd093e1d4b4dd3a0f3dca4aa8a95b0aee8db525a
John Tigue <john@tigue.com>
2015-04-08 03:15:15 -0700

    Migrated from chai to Must.js. Nice.

--- test/caching-geocoder.spec.js
+++ test/caching-geocoder.spec.js
@@ -2,48 +2,41 @@
 
 /** caching-geocoder.js resolves location names to lat&long, uses a cache which can be prefilled from a local file.
   */
-describe('caching-geocoder-spec.js', function(){  
+describe('caching-geocoder.spec.js', function(){  
   'use strict'; 
 
-  var bunyan         = require('bunyan');
-  var logger         = bunyan.createLogger({name: 'myapp'});
+  var bunyan       = require('bunyan');
+  var logger       = bunyan.createLogger({name: 'myapp'});
   logger.level('info');
   
-  var chai           = require('chai');
-  var assert         = chai.assert;
+  var must         = require('must');
 
-
-  //JFT-TODO: want to kill expect off s.t. only using assert interface b/c insane asserting-on-property-access. 
-  //  https://github.com/moll/js-must#asserting-on-property-access
-  //  chai-as-promise is still being used though.
-  var expect         = chai.expect;  
-  
-  // JFT-TOOD: not clear if chaiAsPromised is needed currently (2015-03) as recently mocha has promises built in.
-  // 2015-03-31: this is currently being used though. Search for "eventually". 
-  var chaiAsPromised = require('chai-as-promised');
-  chai.use(chaiAsPromised);
-  
-  var sinon          = require('sinon');
-  var PassThrough    = require('stream').PassThrough;
-  var http           = require('http');
+  var sinon        = require('sinon');
+  var PassThrough  = require('stream').PassThrough;
+  var http         = require('http');
   
   // Actual SUT:
-  var geocoder       = require('../src/geo-librarian/caching-geocoder');
-  
-  
-  // JFT-TODO: this really doesn't know anything about Nominatim. Might want to rename and move this out into a generic test-double-utils.js 
-  
+  var geocoder     = require('../src/geo-librarian/caching-geocoder');
+
   /** Using PassThrough to do test doubling for Nominatim.
     * Following the style of: 
     *   http://codeutopia.net/blog/2015/01/30/how-to-unit-test-nodejs-http-requests/
     * Because cannot just use sinon's simple utilities:
-    *   sinon's fakeServer is not designed to work with node, it's a browser thing (XHR base). Maybe in 2.0 but not now.
+    *   sinon's fakeServer is not designed to work with node, it's a browser only
+    *   thing (XHR-based i.e. it uses its FakeXMLHttpRequest). Maybe in its 2.0 but not now.
     *     https://github.com/cjohansen/Sinon.JS/issues/319
     *     http://stackoverflow.com/questions/26790942/how-to-call-a-fakeserver-in-sinon-js-node
     *       "Sinon is overriding the browser's XMLHttpRequest to create FakeXMLHttpRequest."
-    * So, sinon can be used as test double library in node (as per main page's docs) but the fakeServer bit only works in the browers (weak!)
+    * So, sinon can be used as test double library in node (as per main page's docs) 
+    * but the fakeServer bit only works in the browers (weak!)
+    *
+    * JFT-TODO: sounds like nock is worth checking out: https://github.com/pgte/nock
+    * One way or another, this rigmarole of stubNominatimServer for sinon.stub() and then later
+    * tear down seems a bit repetitive. A mocked HTTP stack would be nice and centralized.
     */
   function stubNominatimServer(aCannedJsonObject){
+    // JFT-TODO: this really doesn't know anything about Nominatim. Might 
+    // want to rename and move this out into a generic test-double-utils.js 
     var aResponse = new PassThrough();
     aResponse.write(JSON.stringify(aCannedJsonObject));
     aResponse.end();
@@ -53,9 +46,9 @@ describe('caching-geocoder-spec.js', function(){
     }
   
 
-  context('when test runner inits this file', function(){
+  context('when mocha loads up this file', function(){
     it('should load caching-geocoder via module system i.e. basic sanity check', function(){
-      assert.isDefined(geocoder);
+      must(geocoder).exist();
       });
     });
 
@@ -63,40 +56,23 @@ describe('caching-geocoder-spec.js', function(){
   context('when asked to geocode Seattle', function(){
     var seattleResult = {};
 
-    before(function(done){
+    before(function(){
       var seattleStub = {
-        wrong: [{lat:'11.11', lon:'22.22'}],
-        right: [{lat:'47.6097', lon:'-122.3331'}] 
+        wrong:   [{lat:'11.11',   lon:'22.22'}],
+        correct: [{lat:'47.6097', lon:'-122.3331'}] 
         };
-      stubNominatimServer(seattleStub.right);
-      geocoder.locate('Seattle').then(function(locations){
+      stubNominatimServer(seattleStub.correct);
+      return geocoder.locate('Seattle').then(function(locations){
         seattleResult.long = parseFloat(locations[0].lon);
         seattleResult.lat  = parseFloat(locations[0].lat);
         logger.debug("in Seattle's then() locs.length=" + locations.length  + " and locs[0].long=" + locations[0].lon + " aka ~" + seattleResult.long);
-        done();
-        }, function(err){done(err);}
-        );
+        });
       });
 
     it('should say Seattle ~ (47°N & 122°W)', function(){
-      // Seattle === 47.6097° N, 122.3331° W
-      assert.closeTo(seattleResult.long, -122.3, 0.5, "Seattle is close to 122.3° W"); 
-      assert.closeTo(seattleResult.lat, 47.6, 0.5, "Seattle is close to 47.6° N"); 
-      });
-
-    it.skip('should say Seattle ~= 47 degrees North (chai as promised)', function(done) {
-      var seattleLatPromise = geocoder.locate('Seattle');
-      seattleLatPromise.then( function(locs) {
-        var seaLat = parseInt(locs[0].lat);
-        logger.debug( 'in SEA test\'s then() locs.length=' + locs.length  + ' and locs[0].lat=' + locs[0].lat + ' aka ~' + seaLat );
-        return seaLat;
-        });
-      logger.debug('seattleLatPromise='+seattleLatPromise);
-      assert.isDefined(seattleLatPromise);
-  
-      // JFT-TODO: something wrong here: (with chai-as-promised?)
-      // TypeError: Cannot read property 'eventually' of undefined
-      return seattleLatPromise.should.eventually.equal(47); //'Seattle is ~47 North'
+      // Seattle ~== 47.6097° N, 122.3331° W
+      must(seattleResult.long).between(-122.8, -121.8);
+      must(seattleResult.lat).between(47.1, 48.1);
       });
   
     after(function(){
@@ -105,28 +81,26 @@ describe('caching-geocoder-spec.js', function(){
     });
 
   
-  context('when asked to geocode a name with embedded spaces "New York City"', function(){
+  context('when asked to geocode a name with embedded spaces ("New York City")', function(){
     var nycStub = {
-      wrong: [{lat:'11.11', lon:'22.22'}],
-      right: [{lat:'40.7127', lon:'-74.0059'}] 
+      wrong: [{lat:'11.11',   lon:'22.22'}],
+      correct: [{lat:'40.7127', lon:'-74.0059'}] 
       };
     var nyc = {};
 
-    before(function(done){
-      stubNominatimServer(nycStub.right);
-      geocoder.locate( "New York City" ).then( function(locations) {
+    before(function(){
+      stubNominatimServer(nycStub.correct);
+      return geocoder.locate( "New York City" ).then( function(locations) {
         nyc.long = parseFloat(locations[0].lon);
         nyc.lat  = parseFloat(locations[0].lat);
         logger.debug("in NYC's then() locs.length=" + locations.length  + " and locs[0].long=" + locations[0].lon + " aka ~" + nyc.long);
-        done();
-        }, function(err){done(err);}
-        );
+        });
       });
 
     it('should deal with the spaces and report NYC\'s location correctly enough', function(){
        // NYC === 40.7127° N, 74.0059° W
-      assert.closeTo(nyc.long, -74, 0.5, "NYC is close to 74° W"); 
-      assert.closeTo(nyc.lat, 40.5, 0.5, "NYC is close to 41° N"); 
+      must(nyc.long).between(-74.5, -73.5);
+      must(nyc.lat).between(40.0, 41.0);
       });
 
     after(function(){
@@ -135,31 +109,34 @@ describe('caching-geocoder-spec.js', function(){
     });
 
 
+  /** Currently this is redundant as doesn't really do anything different from the Seattle case above
+    * It also is clearly from an earlier coding style of mine (notice how the Promise is not returned). C'est la vie.
+    * Not really dead code so cannot hurt to keep it around but if rewriting happens then consider chucking it.
+    */
   context('when asked to geocode Fes', function(){
-    var accurateNominatimAnswerForFes  = [{lat:'34.0341156', lon:'-5.0133482'}, {lat:'34.02813075', lon:'-5.01220890165468'}];
-    var inaccurateNominatimAnswerForFes = [{lat:'11.11', lon:'-22.22'}, {lat:'33.33', lon:'-44.44'}];
-    var fesLong = 0;
-    var fesLat = 0;
+    var fesStub = {
+      wrong:   [{lat:'11.11', lon:'-22.22'},          {lat:'33.33', lon:'-44.44'}],
+      correct: [{lat:'34.0341156', lon:'-5.0133482'}, {lat:'34.02813075', lon:'-5.01220890165468'}]
+      };
+    var fesLoc = {};
 
-    before(function(done){
-      stubNominatimServer(accurateNominatimAnswerForFes);
+    before(function(){
+      stubNominatimServer(fesStub.correct);
 
       var startTime = process.hrtime();
-      geocoder.locate('Fes, Morocco').then( function( locs ) {
-        fesLong = parseFloat(locs[0].lon);
-        fesLat = parseFloat(locs[0].lat);
-        logger.debug( "in Fes's then() locs.length=" + locs.length  + " and locs[0].long=" + locs[0].lon + " aka ~" + fesLong );
+      return geocoder.locate('Fes, Morocco').then( function( locs ) {
+        fesLoc.long = parseFloat(locs[0].lon);
+        fesLoc.lat = parseFloat(locs[0].lat);
+        logger.debug( "in Fes's then() locs.length=" + locs.length  + " and locs[0].long=" + locs[0].lon + " aka ~" + fesLoc.long );
         var firstElapsedMillis = process.hrtime( startTime )[1] / 1000000;
         logger.debug( "lookup of Fes took " + firstElapsedMillis + " milliseconds." );
-        done();
-        }, function(err) {done(err);}
-        );
+        });
       });
  
     it('should respond with approx. correct coords for Fes', function(){
       // Fes, Morocco 34.0333° N, 5.0000° W
-      assert.closeTo(fesLong, -5, 0.1, "Fes is very close to 5.0° W"); 
-      assert.closeTo(fesLat, 34, 0.1, "Fes is very close to 34.0° N"); 
+      must(fesLoc.long).between(-5.2, -4.8);
+      must(fesLoc.lat).between(33.5, 34.5);
       });
 
     after(function(){
@@ -168,14 +145,17 @@ describe('caching-geocoder-spec.js', function(){
     });
   
   
-  // see test/test/tickle-osm-nominatim-geocoder-test.js for the same test (i.e. ask for Bangkok twice) done by actually hitting the network.
+  /** See test/tickle-osm-nominatim-geocoder-test.js for this same 
+    * test (i.e. ask for Bangkok twice) done by actually hitting the 
+    * network. I.e. this is isolated unit test, that is integration testing.
+    */
   context('when asked to geocode the same name twice (Bangkok)', function(){
     function isBangkok(aLoc){
       //Bangkok, Thailand is at roughly 13.7563° N, 100.5018° E
       var aLat = parseFloat(aLoc.lat);
       var aLong = parseFloat(aLoc.lon);
       var answer =  (13.2 < aLat && aLat < 14.2) && (100.0 < aLong && aLong < 101.0); 
-      logger.debug("isBangkok()==" + answer);
+      logger.debug('isBangkok()==' + answer);
       return answer;
       }
 
@@ -184,7 +164,9 @@ describe('caching-geocoder-spec.js', function(){
     var cacheCounts = {};
 
     before(function(done){
-      geocoder.clearCache(); // just incase Bangkok got asked for in some other describe()
+      geocoder.clearCache(); 
+      // ...just incase Bangkok got asked for in some earlier describe()
+
       cacheCounts.init = geocoder.getCacheSize();
       var bangkokStub = {
         wrong: [{lat:'11.11', lon:'22.22'}],
@@ -193,34 +175,57 @@ describe('caching-geocoder-spec.js', function(){
       stubNominatimServer(bangkokStub.correct);
 
       // Ask for Bangkok twice:
+      // JFT-TODO: could do this as returning a new Promise.all([1st, 2nd]) 
+      // ...or would that not ensure sequential behavior i.e. want to test
+      // that first request loads the cache before second one runs. So, chain 
+      // the two requests instead?
       geocoder.locate('Bangkok, Thailand').then(
         function(locations){
           cacheCounts.afterFirstAsk = geocoder.getCacheSize();
           bangkokFirstAsk.location = locations[0];
-          logger.debug("in Bangkok then() #1");  
+          logger.debug('in Bangkok then() #1');  
           // Ask a second time:
+          // JFT-TODO: could make this a chained .then()
           geocoder.locate('Bangkok, Thailand').then(function(locs){
             cacheCounts.afterSecondAsk = geocoder.getCacheSize();
             bangkokSecondAsk.location = locations[0];
-            done();}, 
-            function(err) {done(err);}
+            done();}
             );
-  	  }, 
-        function(err) {done(err);} // JFT-TODO: can this be reduced to just: done
+  	  } 
         );
       });
 
     it('should cache results rather than hit the network twice', function(){ 
       // Check that geocoder returned location of Bangkok twice, 
       //   and more importantly the cache's size grew by one first time and by zero second time.
-      assert(isBangkok(bangkokFirstAsk.location), "first Bangkok ask got wrong result");
-      assert(cacheCounts.init + 1 === cacheCounts.afterFirstAsk, "first Bangkok ask did not increase cache size");
-      assert(isBangkok(bangkokSecondAsk.location), "second Bangkok ask got wrong result");
-      assert(cacheCounts.afterFirstAsk === cacheCounts.afterSecondAsk, "second Bangkok ask did increase cache size");
+      must(isBangkok(bangkokFirstAsk.location)).true();
+      must(cacheCounts.init + 1).equal(cacheCounts.afterFirstAsk);
+      must(isBangkok(bangkokSecondAsk.location)).true();
+      must(cacheCounts.afterFirstAsk).equal(cacheCounts.afterSecondAsk);
       });
 
     after(function(){
       http.request.restore();
+      });
+    });
+
+
+  /** This geocoder is a caching geocoder. The cache consists of a mapping of location names to Promises of
+    * geo-coordinates for the named locations. The cache can be populated 2 ways:
+    *   1. The obvious way is for it to go fetch a name's geo-coordinates via a geocoding service's HTTP REST API.
+    *   2. The less obvious way is for it to be fed pre-canned mappings of a loc name to a geo-coord.
+    * 
+    * The latter case is useful for situations where the location names that will be of interest 
+    * are known ahead of time. In that caase, pre-loading the cache can make for a much quicker 
+    * start-up time compared to having to go across the network for each location name's geo-coordinates.
+    *
+    * In the interest of separation of responsibilities, caching-geocoder.js does not know where
+    * the pre-canned Promises come from; it is handed the Promises and caches them. Something else
+    * is responsible for coming up with the promises, say, by reading their data` from a local file or other store.
+    */
+  context('when set with a pre-fetched name-to-geo-loc Promise', function(){
+    it.skip('must return that Promise when asked to geocode that location name', function(){
+      must.throw(); //JFT-TODO
       });
     });
   });
